88cbab5c5555ef4688a4d6fd05713d077e06cf96
chaz <chaz60795@yahoo.com>
2024-09-02 22:26:48 -0400

    add register form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,34 @@
-// components/Header.js
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import * as AuthSession from "expo-auth-session";
 import config from "../../config/config";
-import axios from "axios";
+import ModalComponent from "./ModalComponent";
 
 const Header = ({ title, onLoginPress }) => {
-  const handleVerify = async () => {
-    try {
-      const response = await axios.post(
-        "https://sandbox-api.va.gov/services/veteran-confirmation/v1/status",
-        {
-          firstName: "Wesley",
-          middleName: "Watson",
-          lastName: "Ford",
-          birthDate: "1986-05-06",
-          gender: "M",
-          streetAddressLine1: "1723 GOSNELL RD",
-          city: "VIENNA",
-          zipCode: "22182",
-          state: "VA",
-          country: "USA",
-        },
-        {
-          headers: {
-            apikey: "rjJDXiyVsmB0qYPjB1Su9hSYGQJAwooy", // Ensure this is correct
-            "Content-Type": "application/json",
-          },
-        }
-      );
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
+  const [showModal, setShowModal] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const handleLoginPress = () => {
+    setShowModal(true);
   };
 
   return (
-    <View style={styles.header}>
-      <Text style={styles.logo}>
-        vet<Text style={styles.logoApp}>era</Text>
-      </Text>
-      {config.showLoginButton && (
-        <TouchableOpacity onPress={handleVerify} style={styles.loginButton}>
-          <Text style={styles.loginButtonText}>Login</Text>
-        </TouchableOpacity>
-      )}
+    <View>
+      <View style={styles.header}>
+        <Text style={styles.logo}>
+          vet<Text style={styles.logoApp}>era</Text>
+        </Text>
+        {config.showLoginButton && (
+          <TouchableOpacity
+            onPress={handleLoginPress}
+            style={styles.loginButton}
+          >
+            <Text style={styles.loginButtonText}>
+              {isLoggedIn ? "Login" : "Join"}
+            </Text>
+          </TouchableOpacity>
+        )}
+      </View>
+      <ModalComponent showModal={showModal} setShowModal={setShowModal} />
     </View>
   );
 };
